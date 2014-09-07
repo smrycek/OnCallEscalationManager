eb5a98c5acda9289937efab2f03e9b534b1fdd65
ldshehan <ldshehan@ncsu.edu>
2014-09-07 17:22:06 -0400

    Added the test for staffMapper.

--- test/TestMappers.js
+++ test/TestMappers.js
@@ -1,5 +1,9 @@
 var assert = require("assert"),
     applicationMapper = require('../lib/mappers/ApplicationMapper.js');
+    staffMapper = require('../lib/mappers/StaffMapper.js'),
+    mongoose = require('mongoose')
+
+var _id = mongoose.Types.ObjectId('4edd40c86762e0fb12000003');
 
 describe('ApplicationMapper', function () {
     describe('#mapModel()', function () {
@@ -12,12 +16,14 @@ describe('ApplicationMapper', function () {
     })
 })
 
-describe('ApplicationMapper', function () {
+describe('StaffMapper', function () {
     describe('#mapModel()', function () {
-        it('test fail', function () {
-            applicationMapper.mapModel("test", "555-555-5555", function (mappedApplication) {
-                assert.equal(mappedApplication.Name, "test");
-                assert.equal(mappedApplication.Phone, "555-666-5555");
+        it('should return staff model with the correct attributes', function () {
+            staffMapper.mapModel("tester", "555-555-5555", false, _id, function (mappedStaff) {
+                assert.equal(mappedStaff.Name, "tester");
+                assert.equal(mappedStaff.Primary, "555-555-5555");
+                assert.equal(mappedStaff.IsFallback, false);
+                assert.equal(mappedStaff.ApplID, _id);
             });
         })
     })
