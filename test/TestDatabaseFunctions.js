21a5710d581a93bd8caad159b744151151fa34fa
smrycek <smrycek@ncsu.edu>
2014-09-08 19:33:35 -0400

    Updated the TestDatabaseFunctions.js file to test the 3 different functions in the applicationMapper file.

--- test/TestDatabaseFunctions.js
+++ test/TestDatabaseFunctions.js
@@ -1,11 +1,31 @@
 var assert = require("assert"),
-    applicationController = require('../lib/controllers/ApplicationController.js'),
     mongoose = require('mongoose'),
-    should = require('chai').should;
+    should = require('chai').should(),
+    nconf = require('nconf');
 
 describe('ApplicationController', function () {
+    before(function (done) {
+        //NConf Configuration
+        nconf.env().file({ file: 'settings.json' });
+
+        //Mongoose Configuration
+        mongoose.connect('mongodb://' + nconf.get('database:db_user') + ':' + nconf.get('database:db_pass') + '@kahana.mongohq.com:10070/app28953073');
+        mongoose.connection.once('connected', function () {
+            done();
+        });
+    });
     describe('#add()', function () {
-        it('Add should make a new application in the database and return that instance or an error', function () {
+        var applicationController = require('../lib/controllers/ApplicationController.js');
+        var Application = require('../lib/models/Application.js');
+
+
+        afterEach(function (done) {
+            Application.remove({ Name: "test" }, function () {
+                done();
+            });
+        });
+
+        it('Should add a new application to the database', function (done) {
             applicationController.add("test", "555-555-5555", function (err, doc) {
                 //check that there is no error
                 should.not.exist(err);
@@ -14,30 +34,69 @@ describe('ApplicationController', function () {
                 //test the data
                 assert.equal(doc.Name, "test");
                 assert.equal(doc.Phone, "555-555-5555");
-                //find the application by name
-                describe('#findByName()', function () {
-                    it('Find the application by its name', function () {
+                done();
+            });
+        });
+    });
+
+    describe('#findByName()', function () {
+        var applicationController = require('../lib/controllers/ApplicationController.js');
+        var Application = require('../lib/models/Application.js');
+
+
+        afterEach(function (done) {
+            Application.remove({ Name: "test" }, function () {
+                done();
+            });
+        });
+
+        it('Should pull a user by name from the database', function (done) {
+            applicationController.add("test", "555-555-5555", function (err, doc) {
+                if (!err) {
+                    applicationController.findByName("test", function (err, doc) {
+                        //check that there is no error
+                        should.not.exist(err);
+                        should.exist(doc);
+                        doc.should.be.an('object');
+                        //test the data
+                        assert.equal(doc.Name, "test");
+                        assert.equal(doc.Phone, "555-555-5555");
+                        done();
+                    });
+                }
+            });
+        });
+    });
+
+    describe('#removeByName()', function () {
+        var applicationController = require('../lib/controllers/ApplicationController.js');
+        var Application = require('../lib/models/Application.js');
+
+
+        afterEach(function (done) {
+            Application.remove({ Name: "test" }, function () {
+                done();
+            });
+        });
+
+        it('Should delete a user by name from the database', function (done) {
+            applicationController.add("test", "555-555-5555", function (err, doc) {
+                if (!err) {
+                    applicationController.removeByName("test", function (err) {
+                        //check that there is no error
+                        should.not.exist(err);
                         applicationController.findByName("test", function (err, doc) {
                             //check that there is no error
                             should.not.exist(err);
-                            should.exist(doc);
-                            doc.should.be.an('object');
-                            //test the data
-                            assert.equal(doc.Name, "test");
-                            assert.equal(doc.Phone, "555-555-5555");
-                            //run the opposite test to remove the application from the database.
-                            describe('#deleteByName()', function () {
-                                it('Delete should remove the application from add', function () {
-                                    applicationController.removeByName("test", function (err) {
-                                        //if no error there is success
-                                        should.not.exist(err);
-                                    });
-                                });
-                            });
+                            //Check that doc is a null since it no longer exists in the database.
+                            should.not.exist(doc);
+                            done();
                         });
                     });
-                });
+                }
             });
         });
+
+
     });
 });
