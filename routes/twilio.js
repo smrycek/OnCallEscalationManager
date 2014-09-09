c5b38bf9b1510497901cf46c84fa1066c64a9eeb
smrycek <smrycek@ncsu.edu>
2014-09-08 22:50:46 -0400

    Forgot to change a post to a get

--- routes/twilio.js
+++ routes/twilio.js
@@ -2,5 +2,5 @@ var Incoming = require('../lib/twilio/Incoming.js')
 
 module.exports = function (app) {
     app.post('/calls/incoming', Incoming.handleIncoming);
-    app.post('/calls/incoming', Incoming.handleIncoming);
+    app.get('/calls/incoming', Incoming.handleIncoming);
 };
