5cef88043f73fb43016fbb89bab4a97ca66878a6
smrycek <smrycek@ncsu.edu>
2014-09-08 22:47:12 -0400

    PhoneFormatter.js - new file  - Created function to format twilio phone number to database formatted.
    TwimlGenerator.js - new file
     - This file will be used to generate twiml used throughout the application.
     - Currently created a dial function for dial twiml.
    
    testFormatters.js - new file
     - Created a test to test the PhoneFormatter file and function.
    
    Incoming.js
     - Modified the handleIncoming function to:
        - convert the incoming number to a correct format.
        - Lookup the application associated with that number.
        - Find the fallback staffer for that application.
        - generate a dial twiml for that fallback staffers number.
    
    Done by: Sean and Shiv

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,5 +1,6 @@
 var Incoming = require('../lib/twilio/Incoming.js')
 
 module.exports = function (app) {
-    app.get('/calls/incoming', Incoming.handleIncoming);
+    app.post('/calls/incoming', Incoming.handleIncoming);
+    app.post('/calls/incoming', Incoming.handleIncoming);
 };
---
+++ test/testFormatters.js
@@ -0,0 +1,16 @@
+var assert = require("assert"),
+    should = require('chai').should();
+
+describe('PhoneFormatter', function () {
+    describe('#twilioToDatabase', function () {
+        var phoneFormatter = require('../lib/formatters/PhoneFormatter.js');
+
+        it('should convert a US phone number from twilio\'s format to a database compatible one. (+19194913313 -> 919-491-3313)', function (done) {
+            phoneFormatter.twilioToDatabase('+19194913313', function (databasePhone) {
+                console.log(databasePhone);
+                should.exist(databasePhone);
+                databasePhone.should.equal('919-491-3313');
+            });
+        });
+    });
+});
