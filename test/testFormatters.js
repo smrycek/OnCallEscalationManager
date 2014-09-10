5d6470ae1453a0a76257b91e33ea8d49f651f411
ldshehan <ldshehan@ncsu.edu>
2014-09-10 13:09:40 -0400

    Renamed to match others tests. Added () to make it uniform.

--- test/testFormatters.js
+++ test/testFormatters.js
@@ -2,7 +2,7 @@ var assert = require("assert"),
     should = require('chai').should();
 
 describe('PhoneFormatter', function () {
-    describe('#twilioToDatabase', function () {
+    describe('#twilioToDatabase()', function () {
         var phoneFormatter = require('../lib/formatters/PhoneFormatter.js');
 
         it('should convert a US phone number from twilio\'s format to a database compatible one. (+19194913313 -> 919-491-3313)', function (done) {
