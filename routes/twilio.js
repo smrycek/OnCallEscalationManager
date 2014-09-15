b8668408d040b178aa43c16c8415329e2f79c804
ldshehan <ldshehan@ncsu.edu>
2014-09-15 16:42:21 -0400

    Added route for redirect and the handler for it.

--- routes/twilio.js
+++ routes/twilio.js
@@ -3,3 +3,7 @@ var twilioHandler = require('../lib/handlers/TwilioHandler.js')
 module.exports = function (app) {
     app.post('/calls/incoming', twilioHandler.handleIncoming);
 };
+
+module.exports = function (app) {
+    app.post('/calls/redirect', twilioHandler.handleRedirect);
+};
