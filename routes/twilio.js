8decbdc28a2817833f5458de6397bf72c1f79d53
smrycek <smrycek@ncsu.edu>
2014-09-15 22:35:53 -0400

    Modified a few things that were causing issues. twilio route was being exported twice. renamed a function in the handlers. Forgot to return a value in the generator.

--- routes/twilio.js
+++ routes/twilio.js
@@ -2,8 +2,5 @@ var twilioHandler = require('../lib/handlers/TwilioHandler.js')
 
 module.exports = function (app) {
     app.post('/calls/incoming', twilioHandler.handleIncoming);
-};
-
-module.exports = function (app) {
     app.post('/calls/redirect', twilioHandler.handleRedirect);
 };
