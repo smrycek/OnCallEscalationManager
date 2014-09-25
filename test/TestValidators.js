06387611f53458ce2672e99b43e9bdb314654a83
ldshehan <ldshehan@ncsu.edu>
2014-09-25 17:38:42 -0400

    Fixed some random syntax things. Started working on the phone validator and its test.

---
+++ test/TestValidators.js
@@ -0,0 +1,13 @@
+var assert = require("assert"),
+    should = require('chai').should();
+
+describe('PhoneValidator: ', function () {
+    descirbe('the validatePhone', function () {
+        it('should take any one of these inputs as valid', function() {
+            
+        });
+        it('should not take any one of these inputs as valid', function() {
+            
+        });
+    });
+});
