ceead2fb04920c221c8862f50acfebb6543c2916
ldshehan <ldshehan@ncsu.edu>
2014-09-25 21:31:14 -0400

    Phone formatter will now put in the format chosen by Doug "(919) 943-3313". This will now be the format displayed and used throughout the program. Fixed the test to reflect this change. This meant adding a function to take the validated digits and format them.

--- test/testFormatters.js
+++ test/testFormatters.js
@@ -5,9 +5,15 @@ describe('PhoneFormatter', function () {
     describe('#twilioToDatabase()', function () {
         var phoneFormatter = require('../lib/formatters/PhoneFormatter.js');
 
-        it('should convert a US phone number from twilio\'s format to a database compatible one. (+19194913313 -> 919-491-3313)', function (done) {
+        it('should convert a US phone number from twilio\'s format to a database compatible one. (+19194913313 -> (919) 491-3313)', function (done) {
             var databasePhone = phoneFormatter.twilioToDatabase('+19194913313');
-            databasePhone.should.equal('919-491-3313');
+            databasePhone.should.equal('(919) 491-3313');
+            done();
+        });
+
+        it('should convert a US phone number from 10 digits to a database compatible one. (9194913313 -> (919) 491-3313)', function (done) {
+            var databasePhone = phoneFormatter.digitsToDatabase('9194913313');
+            databasePhone.should.equal('(919) 491-3313');
             done();
         });
     });
