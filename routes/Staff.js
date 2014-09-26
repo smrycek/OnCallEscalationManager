ec49c0409b994d9540ec0867693b4c18f5275d72
smrycek <smrycek@ncsu.edu>
2014-09-26 15:24:21 -0400

    Small Fix.

--- routes/Staff.js
+++ routes/Staff.js
@@ -2,5 +2,5 @@ var staffHandler = require('../lib/handlers/StaffHandler.js')
 
 module.exports = function (app) {
     app.post('/api/staff/add', staffHandler.handleAdd);
-    app.post('/api/staff/remove', staffHandler.handleRemove);
+    app.post('/api/staff/remove', staffHandler.handleDelete);
 };
