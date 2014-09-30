7a66403cf1570b40ad072957b91fd0dcd6f820ec
ldshehan <ldshehan@ncsu.edu>
2014-09-30 17:48:01 -0400

    Added populate to application controller to populate the fallback staffers. Added angular implementation to the index page for displaying all groups.

--- html/includes/angular/Controllers.js
+++
@@ -1,9 +0,0 @@
-function IndexCtrl($scope, $http) {
-    $http.get('/api/applications/').
-        success(function (data, status, headers, config) {
-            var app = new Object();
-            app.Name = 'test';
-            app.Phone = '(555) -555-5555';
-            $scope.app = app;
-        });
-}
--- html/includes/angular/Routes.js
+++ html/includes/angular/Routes.js
@@ -1,6 +1,17 @@
-angular.module('OnCallEscalator', [])
-.controller('index', [
-'$scope',
-function($scope){
-    $scope.test = 'Hello world!';
-}]);
+var OCEM = angular.module('OnCallEscalationManager', []);
+
+OCEM.controller('index', ['$scope','$http', indexCtrl]);
+
+function indexCtrl($scope, $http) { 
+    $scope.method = 'GET';
+    $scope.url = '/api/applications/';
+    $http({method: $scope.method, url: $scope.url}).
+        success(function(data, status) {
+          $scope.status = status;
+          $scope.apps = data.results;
+        }).
+        error(function(data, status) {
+          $scope.apps = data.results || "Request failed";
+          $scope.status = status;
+      });
+    };
