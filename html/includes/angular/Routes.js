3467d82d6f4d446b3fe3eae5da7ab3684f2e951c
smrycek <smrycek@ncsu.edu>
2014-10-01 13:16:07 -0400

    Continued work on API - Deleting/Adding now working. Updating still needs to be done for staff.
    Worked with Dalton to get Angular routing working.

--- html/includes/angular/Routes.js
+++ html/includes/angular/Routes.js
@@ -1,19 +1,57 @@
-var OCEM = angular.module('OnCallEscalationManager', []);
+var OCEM = angular.module('OnCallEscalationManager', ['ngRoute']);
 
-OCEM.controller('index', ['$scope','$http', indexCtrl]);
+OCEM.controller('indexCtlr', ['$scope','$http', indexCtrl]);
+OCEM.controller('appCtlr', ['$scope','$http', '$routeParams', appCtrl]);
+
+
+OCEM.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider
+        .when('/', {
+            templateUrl: '/partials/Index.jade',
+            controller: 'indexCtlr'
+        })
+        .when('/Applications/:appName', {
+            templateUrl: '/partials/Index.jade',
+            controller: 'appCtlr'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+  }]);
+
 
 function indexCtrl($scope, $http) {
     $scope.colorCount = 5;
-
     $scope.method = 'GET';
     $scope.url = '/api/applications/';
+
     $http({method: $scope.method, url: $scope.url}).
         success(function(data, status) {
-          $scope.status = status;
-          $scope.apps = data.results;
-        }).
-        error(function(data, status) {
-          $scope.apps = data.results || "Request failed";
-          $scope.status = status;
-      });
-    };
+        $scope.status = status;
+        $scope.apps = data.results;
+    })
+    .error(function(data, status) {
+        $scope.apps = data.results || "Request failed";
+        $scope.status = status;
+    });
+};
+
+function appCtrl($scope, $http, $routeParams) {
+    $scope.params = $routeParams;
+
+    $scope.colorCount = 5;
+    $scope.method = 'GET';
+    $scope.url = '/api/applications/';
+
+    $http({method: $scope.method, url: $scope.url}).
+        success(function(data, status) {
+        $scope.status = status;
+        $scope.apps = data.results;
+    })
+    .error(function(data, status) {
+        $scope.apps = data.results || "Request failed";
+        $scope.status = status;
+    });
+};
--- routes/Gui.js
+++ routes/Gui.js
@@ -1,5 +1,21 @@
 module.exports = function (app) {
-    app.get('/', function (req, res) {
+    /*app.get('/', function (req, res) {
         res.render('Index');
+    });
+    app.get('/Applications/:appName', function (req, res) {
+        res.render('Index');
+    });*/
+
+    app.get('/', function (req, res) {
+        res.render('layout');
+    });
+
+    app.get('/Applications/:appName', function (req, res) {
+        res.render('layout');
+    });
+
+    app.get('/partials/:name', function (req, res) {
+        var name = req.params.name;
+        res.render(name);
     });
 };
