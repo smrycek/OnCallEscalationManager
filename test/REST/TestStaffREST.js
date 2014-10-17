6834c6f269b67c4492d712d714c0aedb108b3b74
smrycek <smrycek@ncsu.edu>
2014-10-17 17:00:00 -0400

    Finished up testing for StaffHandler. It seems to be as high as I can get it. The few untested sections are due to .fails of q.

--- test/REST/TestStaffREST.js
+++ test/REST/TestStaffREST.js
@@ -462,130 +462,244 @@ var assert = require("assert"),
             });
         });
 
-        /*
+        
         describe('PUT Request', function () {
 
 
-        beforeEach(function (done) {
-        var application = {
-        Name: 'test',
-        Phone: '9194913313',
-        Fallback: '4edd40c86762e0fb12000003'
-        };
+            beforeEach(function (done) {
+                var application = {
+                    Name: 'test',
+                    Phone: '9194913313',
+                    Fallback: '4edd40c86762e0fb12000003'
+                };
 
-        applicationController.add(application, function (err, doc) {
-        done();
-        });
-        });
+                var staffToAdd = new Object();
+                staffToAdd.Name = "testStaff";
+                staffToAdd.Primary = "666-666-6666";
 
-        afterEach(function (done) {
-        Application.remove({ Name: "test" }, function () {
-        done();
-        });
-        });
+                applicationController.add(application, function (err, doc) {
+                    staffController.add(staffToAdd, function(err, staff) {
+                        applicationController.addToStaff(doc._id, staff, function(err, doc) {
+                            done();
+                        })
+                    })
+                });
+            });
 
-        it('should return an error if it cant find the supplied application name', function (done) {
-        var application = {
-        Phone: '9194913312',
-        Fallback: '4edd40c86762e0fb12000003'
-        };
+            afterEach(function (done) {
+                Application.remove({}, function () {
+                    Staff.remove({}, function () {
+                        done();
+                    });
+                });
+            });
 
-        request(app)
-        .put('/api/applications/FakeApp')
-        .send(application)
-        .expect('Content-Type', /json/)
-        .expect(500) //Status code
-        .end(function (err, res) {
-        if (err) {
-        throw err;
-        }
-        res.body.should.have.property('Message');
-        res.body.Message.should.equal('Unable to find an application with the name FakeApp.');
-        res.body.Status.should.equal('Error');
-        done();
-        });
-        });
 
-        it('should return an error if the new phone number given is invalid', function (done) {
-        var application = {
-        Phone: '919491331',
-        Fallback: '4edd40c86762e0fb12000003'
-        };
+            it('should return an error if the application with the name given does not exist', function (done) {
+                var staffer = {
+                    Name: 'testStaff',
+                    Phone: '666-666-6666'
+                };
 
-        request(app)
-        .put('/api/applications/test')
-        .send(application)
-        .expect('Content-Type', /json/)
-        .expect(500) //Status code
-        .end(function (err, res) {
-        if (err) {
-        throw err;
-        }
-        res.body.should.have.property('Message');
-        res.body.Message.should.equal('Number did not consist of 10 digits.');
-        res.body.Status.should.equal('Error');
-        done();
-        });
-        });
+                request(app)
+                .put('/api/applications/FakeApp/staff/666-666-6666/')
+                .send(staffer)
+                .expect('Content-Type', /json/)
+                .expect(500) //Status code
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.body.should.have.property('Message');
+                    res.body.Message.should.equal('Application FakeApp does not exist.');
+                    res.body.Status.should.equal('Error');
+                    done();
+                });
+            });
 
-        it('should update an application in the database if all inputs are correct', function (done) {
-        var application = {
-        Phone: '9194913312',
-        Fallback: '4edd40c86762e0fb12000003'
-        };
+            it('should return an error if no staff member with the number given exists', function (done) {
+                var staffer = {
+                    Name: 'testStaff',
+                    Phone: '666-666-6666'
+                };
 
-        request(app)
-        .put('/api/applications/test')
-        .send(application)
-        .expect('Content-Type', /json/)
-        .expect(200) //Status code
-        .end(function (err, res) {
-        if (err) {
-        throw err;
-        }
-        res.body.should.have.property('Name');
-        res.body.Name.should.equal('test');
-        res.body.Status.should.equal('Success');
-        done();
-        });
-        });
+                request(app)
+                .put('/api/applications/test/staff/666-666-6667/')
+                .send(staffer)
+                .expect('Content-Type', /json/)
+                .expect(500) //Status code
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.body.should.have.property('Message');
+                    res.body.Message.should.equal('No staff member found with this phone number.');
+                    res.body.Status.should.equal('Error');
+                    done();
+                });
+            });
+
+            it('should return an error if the new phone number is invalid', function (done) {
+                var staffer = {
+                    Name: 'testStaff',
+                    Phone: '666-666-666'
+                };
+
+                request(app)
+                .put('/api/applications/test/staff/666-666-6666/')
+                .send(staffer)
+                .expect('Content-Type', /json/)
+                .expect(500) //Status code
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.body.should.have.property('Message');
+                    res.body.Message.should.equal('Number did not consist of 10 digits.');
+                    res.body.Status.should.equal('Error');
+                    done();
+                });
+            });
+
+            it('should update a staff member if all input is correct', function (done) {
+                var staffer = {
+                    Name: 'testStaff2',
+                    Phone: '666-666-6666'
+                };
+
+                request(app)
+                .put('/api/applications/test/staff/666-666-6666/')
+                .send(staffer)
+                .expect('Content-Type', /json/)
+                .expect(200) //Status code
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.body.should.have.property('id');
+                    res.body.should.have.property('Name');
+                    res.body.Name.should.equal('testStaff2');
+                    res.body.Status.should.equal('Success');
+                    done();
+                });
+            });
         });
 
         describe('DELETE Request', function () {
 
-        beforeEach(function (done) {
-        var application = {
-        Name: 'test',
-        Phone: '9194913313',
-        Fallback: '4edd40c86762e0fb12000003'
-        };
+            afterEach(function (done) {
+                Application.remove({ Name: "test" }, function () {
+                    done();
+                });
+            });
 
-        applicationController.add(application, function (err, doc) {
-        done();
-        });
-        });
+            var staffToAdd = new Object();
+            staffToAdd.Name = "testStaff";
+            staffToAdd.Primary = "666-666-6666";
 
-        afterEach(function (done) {
-        Application.remove({ Name: "test" }, function () {
-        done();
-        });
-        });
+            var application = {
+                Name: 'test',
+                Phone: '9194913313',
+                Fallback: '4edd40c86762e0fb12000003'
+            };
 
-        it('should delete the application name supplied in the URL', function (done) {
-        request(app)
-        .delete('/api/applications/test')
-        .expect('Content-Type', /json/)
-        .expect(200) //Status code
-        .end(function (err, res) {
-        if (err) {
-        throw err;
-        }
-        res.body.should.have.property('Identifier');
-        res.body.Identifier.should.equal('test');
-        res.body.Status.should.equal('Success');
-        done();
-        });
-        });
-        });*/
+            var _id;
 
+            it('should delete the staff member with the supplied phone number from the application', function (done) {
+                applicationController.add(application, function (err, doc) {
+                    _id = doc._id;
+                    staffController.add(staffToAdd, function (err, staff) {
+                        request(app)
+                        .delete('/api/applications/test/staff/666-666-6666/')
+                        .expect('Content-Type', /json/)
+                        .expect(200) //Status code
+                        .end(function (err, res) {
+                            if (err) {
+                                throw err;
+                            }
+                            res.body.should.have.property('results');
+                            res.body.Status.should.equal('Success');
+
+                            applicationController.containsStaff(_id, staff, function(err, exists) {
+                               exists.should.be.false;
+                               done();
+                            });
+                        });
+                    });
+                });
+            });
+
+            it('should return an error if the application name given doesnt exist', function (done) {
+                var toAdd = new Object();
+                toAdd.Name = "test";
+                toAdd.Phone = "555-555-5555";
+
+                var staffToAdd = new Object();
+                staffToAdd.Name = "testStaff";
+                staffToAdd.Phone = "666-666-6666";
+
+                applicationController.add(toAdd, function (err, doc) {
+                    //check that there is no error
+                    should.not.exist(err);
+                    should.exist(doc);
+                    doc.should.be.an('object');
+                    //test the data
+                    assert.equal(doc.Name, "test");
+                    assert.equal(doc.Phone, "(555) 555-5555");
+
+
+                    request(app)
+                    .delete('/api/applications/test2/staff/666-666-6666/')
+                    .send(staffToAdd)
+                    .expect('Content-Type', /json/)
+                    .expect(500) //Status code
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('Message');
+                        res.body.Message.should.equal('Application test2 does not exist.');
+                        res.body.Status.should.equal('Error');
+                        done();
+                    });
+                });
+            });
+
+            it('should return an error if there is no staff member with the number given', function (done) {
+                var toAdd = new Object();
+                toAdd.Name = "test";
+                toAdd.Phone = "555-555-5555";
+
+                var staffToAdd = new Object();
+                staffToAdd.Name = "testStaff";
+                staffToAdd.Phone = "666-666-6666";
+
+                applicationController.add(toAdd, function (err, doc) {
+                    //check that there is no error
+                    should.not.exist(err);
+                    should.exist(doc);
+                    doc.should.be.an('object');
+                    //test the data
+                    assert.equal(doc.Name, "test");
+                    assert.equal(doc.Phone, "(555) 555-5555");
+
+
+                    request(app)
+                    .delete('/api/applications/test/staff/666-666-6667/')
+                    .send(staffToAdd)
+                    .expect('Content-Type', /json/)
+                    .expect(500) //Status code
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.have.property('Message');
+                        res.body.Message.should.equal('No staff member found with this phone number.');
+                        res.body.Status.should.equal('Error');
+                        done();
+                    });
+                });
+            });
+
+        });
     });
