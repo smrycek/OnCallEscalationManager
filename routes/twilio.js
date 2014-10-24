cd75fcd6c44020ae1864c69cb5a6280a2db14634
smrycek <smrycek@ncsu.edu>
2014-10-24 15:01:52 -0400

    Added route and base setup for call forwarding

--- routes/twilio.js
+++ routes/twilio.js
@@ -3,6 +3,7 @@ var twilioHandler = require('../lib/handlers/TwilioHandler.js')
 module.exports = function (app) {
     app.post('/calls/incoming', twilioHandler.handleIncoming);
     app.post('/calls/redirect', twilioHandler.handleRedirect);
+    app.get('/calls/screen/:confName', twilioHandler.handleConference);
     app.get('/calls/conference/:confName', twilioHandler.handleConference);
     app.post('/calls/redirect/secondary', twilioHandler.handleSecondary);
     app.post('/calls/redirect/fallback', twilioHandler.handleFallback);
