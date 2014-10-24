daf1f69dca8e4608e581d9d25631ac8a767fe0ac
smrycek <smrycek@ncsu.edu>
2014-10-24 15:51:47 -0400

    forgot to remove unused routes.

--- routes/twilio.js
+++ routes/twilio.js
@@ -6,6 +6,6 @@ module.exports = function (app) {
     app.get('/calls/screen/:confName/:screening', twilioHandler.handleScreen);
     app.get('/calls/conference/:confName', twilioHandler.handleConference);
     app.post('/calls/conference/:confName/:screening', twilioHandler.handleConference);
-    app.post('/calls/redirect/secondary', twilioHandler.handleSecondary);
-    app.post('/calls/redirect/fallback', twilioHandler.handleFallback);
+    //app.post('/calls/redirect/secondary', twilioHandler.handleSecondary);
+    //app.post('/calls/redirect/fallback', twilioHandler.handleFallback);
 };
