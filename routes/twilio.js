73eea3e8ea2b8c37b0da51bfcdf70557894653b2
shpatel5 <shpatel5@ncsu.edu>
2014-10-24 14:18:02 -0400

    Changed the routes and handlers to handle the calls of the primary and secondary.

--- routes/twilio.js
+++ routes/twilio.js
@@ -4,4 +4,6 @@ module.exports = function (app) {
     app.post('/calls/incoming', twilioHandler.handleIncoming);
     app.post('/calls/redirect', twilioHandler.handleRedirect);
     app.get('/calls/conference/:confName', twilioHandler.handleConference);
+    app.post('/calls/redirect/secondary', twilioHandler.handleSecondary);
+    app.post('/calls/redirect/fallback', twilioHandler.handleFallback);
 };
