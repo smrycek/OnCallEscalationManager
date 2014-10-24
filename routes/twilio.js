07984c0fe54e977c7baa2a9e6b7ccfba7e041b7f
smrycek <smrycek@ncsu.edu>
2014-10-24 15:49:09 -0400

    Editedthe handleConference function to redirect through the call heirarchy.  Added some new twiml.  Renamed the old file in case this way of working with isMachine doesn't work.

--- routes/twilio.js
+++ routes/twilio.js
@@ -3,8 +3,9 @@ var twilioHandler = require('../lib/handlers/TwilioHandler.js')
 module.exports = function (app) {
     app.post('/calls/incoming', twilioHandler.handleIncoming);
     app.post('/calls/redirect', twilioHandler.handleRedirect);
-    app.get('/calls/screen/:confName', twilioHandler.handleConference);
+    app.get('/calls/screen/:confName/:screening', twilioHandler.handleScreen);
     app.get('/calls/conference/:confName', twilioHandler.handleConference);
+    app.post('/calls/conference/:confName/:screening', twilioHandler.handleConference);
     app.post('/calls/redirect/secondary', twilioHandler.handleSecondary);
     app.post('/calls/redirect/fallback', twilioHandler.handleFallback);
 };
