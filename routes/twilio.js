714306e59f6d4201cbd6fcb166f8825f1eeec130
smrycek <smrycek@ncsu.edu>
2014-10-26 16:57:02 -0400

    Realized a route was messed up which is why an application error was occuring with twilio.

--- routes/twilio.js
+++ routes/twilio.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
     app.post('/calls/redirect', twilioHandler.handleRedirect);
     app.get('/calls/screen/:confName/:screening', twilioHandler.handleScreen);
     app.get('/calls/conference/:confName', twilioHandler.handleConference);
-    app.post('/calls/conference/:confName/:screening', twilioHandler.handleConference);
+    app.get('/calls/conference/:confName/:screening', twilioHandler.handleConference);
     //app.post('/calls/redirect/secondary', twilioHandler.handleSecondary);
     //app.post('/calls/redirect/fallback', twilioHandler.handleFallback);
 };
